9cf22b491a79837f4ab2bf87b0cf633c3f8d79bb
Ha1fmoon <88141314+Ha1fmoon@users.noreply.github.com>
2021-10-01 20:55:52 +0300

    Updated $db_pass variable value

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
   $db_ip = "localhost";
   $db_name = "php_test";
-  $db_pass = "Admin1!";
+  $db_pass = "[enter_your_password_here]";
   $db_user = "root";
   
   $data_base = new mysqli( $db_ip, $db_user, $db_pass, $db_name );
